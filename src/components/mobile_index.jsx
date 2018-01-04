0fb4547e48c1ef7fe54823eac7ec94c374defdf1
fx109138 <1091381572@qq.com>
2018-01-04 18:02:27 +0800

    mobile news finished

--- src/components/mobile_index.jsx
+++ src/components/mobile_index.jsx
@@ -1,31 +1,49 @@
 import React, {Component} from 'react';
 import MobileHeader from './mobile_header';
 import MobileFooter from './mobile_footer';
-import {
-  Tabs
-} from 'antd';
+import MobileList from './mobile_list';
+import {Tabs, Carousel} from 'antd';
+import carousel_1 from '../images/carousel_1.jpg';
+import carousel_2 from '../images/carousel_2.jpg';
+import carousel_3 from '../images/carousel_3.jpg';
+import carousel_4 from '../images/carousel_4.jpg';
 const TabPane = Tabs.TabPane;
 
 export default class MobileIndex extends Component {
   render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      autoplay: true
+    };
     return (
       <div>
         <MobileHeader></MobileHeader>
         <Tabs>
           <TabPane tab="头条" key="1">
-
+            <div className="carousel">
+              <Carousel {...settings}>
+                <div><img src={carousel_1}/></div>
+                <div><img src={carousel_2}/></div>
+                <div><img src={carousel_3}/></div>
+                <div><img src={carousel_4}/></div>
+              </Carousel>
+            </div>
+            <MobileList count={20} type="top"/>
           </TabPane>
           <TabPane tab="社会" key="2">
-
+            <MobileList count={20} type="shehui"/>
           </TabPane>
           <TabPane tab="国内" key="3">
-
+            <MobileList count={20} type="guonei"/>
           </TabPane>
           <TabPane tab="国际" key="4">
-
+            <MobileList count={20} type="guoji"/>
           </TabPane>
           <TabPane tab="娱乐" key="5">
-
+            <MobileList count={20} type="yule"/>
           </TabPane>
         </Tabs>
         <MobileFooter></MobileFooter>
