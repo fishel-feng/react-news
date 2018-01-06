9e6f1bf8da29d98502c55a67ba1dd6c5196645dd
fx109138 <1091381572@qq.com>
2018-01-06 17:40:57 +0800

    list finished

--- src/components/mobile_usercenter.jsx
+++ src/components/mobile_usercenter.jsx
@@ -22,7 +22,49 @@ const FormItem = Form.Item;
 const MenuItemGroup = Menu.ItemGroup;
 
 export default class MobileUserCenter extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      usercollection: '',
+      usercomments: '',
+      previewImage: '',
+      previewVisible: false
+    }; 
+  }
+
+  componentDidMount() {
+		const myFetchOptions = {
+			method: 'GET'
+		};
+		fetch(`http://newsapi.gugujiankong.com/Handler.ashx?action=getuc&userid=${localStorage.userid}`, myFetchOptions).then(response=>response.json()).then(json=>{
+			this.setState({usercollection: json});
+    });
+    fetch(`http://newsapi.gugujiankong.com/Handler.ashx?action=getusercomments&userid=${localStorage.userid}`, myFetchOptions).then(response=>response.json()).then(json=>{
+			this.setState({usercomments: json});
+		});
+	};
+  
   render() {
+    const {usercollection, usercomments} = this.state;
+    const usercollectionList = usercollection.length
+      ? usercollection.map((uc, index) => (
+        <Card key={index} title={uc.uniquekey} extra={<a target="_blank" href={`/#/details/${uc.uniquekey}`}>查看</a>}>
+          <p>
+            {uc.Title}
+          </p>
+        </Card>
+      ))
+      : '没有收藏任何新闻' ;
+    const usercommentsList = usercomments.length
+      ? usercomments.map((comment, index) => (
+        <Card key={index} title={`${comment.datetime} ${comment.uniquekey}`} extra={<a href={`/#/details/${comment.uniquekey}`}>查看</a>}>
+          <p>
+            {comment.Comments}
+          </p>
+        </Card>
+      ))
+      : '没有发表过评论' ;
     return (
       <div>
         <MobileHeader></MobileHeader>
@@ -30,10 +72,18 @@ export default class MobileUserCenter extends Component {
           <Col span={24}>
             <Tabs>
               <TabPane tab="我的收藏列表" key="1">
-
+                <Row>
+                  <Col span={24}>
+                    {usercollectionList}
+                  </Col>
+                </Row>                
               </TabPane>
               <TabPane tab="我的评论列表" key="2">
-
+                <Row>
+                  <Col span={24}>
+                    {usercommentsList}
+                  </Col>
+                </Row>
               </TabPane>
               <TabPane tab="头像设置" key="3">
 
--- src/components/pc_usercenter.jsx
+++ src/components/pc_usercenter.jsx
@@ -14,7 +14,8 @@ import {
   Checkbox,
   Modal,
   Card,
-  notification
+  notification,
+  Upload
 } from 'antd';
 const SubMenu = Menu.SubMenu;
 const TabPane = Tabs.TabPane;
@@ -22,7 +23,68 @@ const FormItem = Form.Item;
 const MenuItemGroup = Menu.ItemGroup;
 
 export default class PCUserCenter extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      usercollection: '',
+      usercomments: '',
+      previewImage: '',
+      previewVisible: false
+    }; 
+  }
+  
+  componentDidMount() {
+		const myFetchOptions = {
+			method: 'GET'
+		};
+		fetch(`http://newsapi.gugujiankong.com/Handler.ashx?action=getuc&userid=${localStorage.userid}`, myFetchOptions).then(response=>response.json()).then(json=>{
+			this.setState({usercollection: json});
+    });
+    fetch(`http://newsapi.gugujiankong.com/Handler.ashx?action=getusercomments&userid=${localStorage.userid}`, myFetchOptions).then(response=>response.json()).then(json=>{
+			this.setState({usercomments: json});
+		});
+	};
+
   render() {
+    const props = {
+			action: 'http://newsapi.gugujiankong.com/handler.ashx',
+			headers: {
+				"Access-Control-Allow-Origin": "*"
+			},
+			listType: 'picture-card',
+			defaultFileList: [
+				{
+					uid: -1,
+					name: 'xxx.png',
+					state: 'done',
+					url: 'https://os.alipayobjects.com/rmsportal/NDbkJhpzmLxtPhB.png',
+					thumbUrl: 'https://os.alipayobjects.com/rmsportal/NDbkJhpzmLxtPhB.png'
+				}
+			],
+			onPreview: (file) => {
+				this.setState({previewImage: file.url, previewVisible: true});
+			}
+    };
+    const {usercollection, usercomments} = this.state;
+    const usercollectionList = usercollection.length
+      ? usercollection.map((uc, index) => (
+        <Card key={index} title={uc.uniquekey} extra={<a target="_blank" href={`/#/details/${uc.uniquekey}`}>查看</a>}>
+          <p>
+            {uc.Title}
+          </p>
+        </Card>
+      ))
+      : '没有收藏任何新闻' ;
+    const usercommentsList = usercomments.length
+      ? usercomments.map((comment, index) => (
+        <Card key={index} title={`${comment.datetime} ${comment.uniquekey}`} extra={<a target="_blank" href={`/#/details/${comment.uniquekey}`}>查看</a>}>
+          <p>
+            {comment.Comments}
+          </p>
+        </Card>
+      ))
+      : '没有发表过评论' ;
     return (
       <div>
         <PCHeader></PCHeader>
@@ -31,13 +93,33 @@ export default class PCUserCenter extends Component {
           <Col span={20}>
             <Tabs>
               <TabPane tab="我的收藏列表" key="1">
-
+                <div className="comment">
+                  <Row>
+                    <Col span={24}>
+                      {usercollectionList}
+                    </Col>
+                  </Row>
+                </div>
               </TabPane>
               <TabPane tab="我的评论列表" key="2">
-
+                <div className="comment">
+                  <Row>
+                    <Col span={24}>
+                      {usercommentsList}
+                    </Col>
+                  </Row>
+                </div>
               </TabPane>
               <TabPane tab="头像设置" key="3">
-
+                <div className="clearfix">
+                  <Upload {...props}>
+                    <Icon type="plus"/>
+                    <div className="ant-upload-text">上传照片</div>
+                  </Upload>
+                  <Modal visible={this.state.previewVisible} footer={null} onCancel={this.handleCancel}>
+                    <img src={this.state.previewImage} alt="预览"/>
+                  </Modal>
+                </div>
               </TabPane>
             </Tabs>
           </Col>
